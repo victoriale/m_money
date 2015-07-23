5fa7299c3d6950acb4954238287d4da529b6be88
Prashanth <prashanth@passfail.com>
2015-07-22 23:19:42 -0500

    Trending News Safari Fix
    Trending news safari Fix

---
+++ client/templates/modules/trending_news/trending_news.js
@@ -0,0 +1,19 @@
+
+Template.trending_news.onRendered( function() {
+  Session.set("trending_news");
+});
+  Template.trending_news.helpers (
+    {
+    trnd_name: function() {
+      var data = Session.get("trending_news");
+      return ["Mark Zuckerberg emerges as business person of the year"];
+    },
+    trnd_writtenby:function() {
+      var data = Session.get("trending_news");
+      return ["Julie Balise"];
+    },
+    trnd_timestamp:function() {
+      var data = Session.get("trending_news");
+      return ["8:17am, Friday July 17th, 2015"];
+    }
+  })
