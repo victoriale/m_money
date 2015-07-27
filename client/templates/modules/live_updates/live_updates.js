dfeace79433b95e6b4712d7c0a9949a69464ff0e
navyae4 <neetaram@geronimo.atai.lan>
2015-07-27 16:51:13 -0500

    live_updates

---
+++ client/templates/modules/live_updates/live_updates.js
@@ -0,0 +1,19 @@
+/*Author: [Naveen]
+Created: [7/23/2015]
+Description: [ This file includes js script for module live-updates along with comments (File name: lives_updates.less)]
+Associated Files: [The modules.less, live_updates.html for the module]*/
+
+Template.live_updates.helpers({
+  block:[/*contains a data for object */
+
+    {title:'7033 N Kedzie Ave #806',went:'went on market May 5,2015,',withh:'with an asking pice of',price:'$124,900.',bed:' 2 Bed ',red:'',bath:' 2 Bath ',sq:' 1500 sq ft ',chic:'chicago,IL',updated:'updated 2 seconds ago'},
+    {title:'3314 W Sunnyside Ave #3',went:'went on market May 10,2015,',withh:'with an asking pice of',price: '$175,000.' ,bed:' 2 Bed ',red:'',bath:'1 Bath',sq:' 1000 sq ft ',chic:'chicago, IL',updated:'updated 10 seconds ago'},
+    {title:'2614 N Clybourn Ave #402',went:'went on market May 8,2015,',withh:'with an asking pice of',price: '$325,000.' ,bed:' 2 Bed ',red:'',bath:'2 Bath',sq:' n/a sq ft ',chic:'chicago, IL',updated:'updated 15 seconds ago'}
+  ],
+  dotted:[ /*contains a data for dottted object */
+    {circle:' fa-circle'},
+    {circle:' fa-circle'},
+    {circle:' fa-circle'},
+  ]
+
+});
