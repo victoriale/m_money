ca8c57f112699e3dd4b8b6e34d148d09e7c1d86c
navyae4 <neetaram@geronimo.atai.lan>
2015-08-04 13:55:27 -0500

    featured_list

--- client/templates/modules/featured_list/featured_list.js
+++ client/templates/modules/featured_list/featured_list.js
@@ -2,13 +2,13 @@
 Author: Meghana yerramilli
 Created: [07-20-2015]
 Description: featured top 100 lists
-Associated Files: featured_top_100.html, featured_top_100.less
+Associated Files: featured_list.html, featured_list.less
 */
 //render function to set the array with data
-Template.featured_top_100.onRendered( function() {
+Template.featured_list.onRendered( function() {
   var counter=1;
   Session.set("counter",counter);
-  Session.set("top100_data",
+  Session.set("fl_data",
   [
     {
       company_name:"Facebook,Inc",
@@ -17,14 +17,14 @@ Template.featured_top_100.onRendered( function() {
       company_stock:"FB",
       company_shares:"19,000,000",
       title:"Facebook's Trending Lists",
-      top100_none:'none',
+      fl_none:'none',
       tile2_icon_none:'none',
       tile3_icon_none:'none'
 
     },
     {
       title:"Company Top 100 Lists",
-      top100_none:'none',
+      fl_none:'none',
       tile1_icon_none:'none',
       tile3_icon_none:'none'
     },
@@ -38,26 +38,26 @@ Template.featured_top_100.onRendered( function() {
 
 });
 //helper function to retrieve data from array
-Template.featured_top_100.helpers (
+Template.featured_list.helpers (
   {
     company_name: function() {
-      var data = Session.get("top100_data");
+      var data = Session.get("fl_data");
       return data[0].company_name;
     },
     company_location: function() {
-      var data = Session.get("top100_data");
+      var data = Session.get("fl_data");
       return data[0].company_location;
     },
     company_CEO: function() {
-      var data = Session.get("top100_data");
+      var data = Session.get("fl_data");
       return data[0].company_CEO;
     },
     company_stock: function() {
-      var data = Session.get("top100_data");
+      var data = Session.get("fl_data");
       return data[0].company_stock;
     },
     company_shares: function() {
-      var data = Session.get("top100_data");
+      var data = Session.get("fl_data");
       return data[0].company_shares;
     },
     counter: function() {
@@ -65,7 +65,7 @@ Template.featured_top_100.helpers (
       return data;
     },
     gettitle: function() {
-      var data = Session.get("top100_data");
+      var data = Session.get("fl_data");
       return data;
     },
   });
