819294c1caf3e242ebe35d8ed2d7367207f969b3
Prashanth <prashanth@passfail.com>
2015-09-16 11:17:40 -0500

    TASK-363(about_us_page) files committed
    about_us_page.html, about_us_page.js about_us_page.less files committed

---
+++ client/templates/webpages/about_us_page/about_us_page_logic.js
@@ -0,0 +1,27 @@
+Template.about_us_page.helpers({
+
+  country: function(){
+    var name0= "United States";
+    return name0;
+  },
+
+  statement: function(){
+    var name0= "Take a Seat and get to know us better.";
+    return name0;
+  },
+
+  // total: function(){
+  //   var name0= "Total U.S. Public Companies";
+  //   return name0;
+  // },
+  //
+  // number: function(){
+  //   var name0= "9,278";
+  //   return name0;
+  // },
+
+items: [
+  {totals:"Total U.S. Public Companies", numbers:"9,278",image:"Icon_Companies.png", totals2:"Total U.S. Executives", numbers2:"101,200", image2:"Icon_Executives.png"},
+  {totals:"Total U.S. Brokerage Firms", numbers:"5,006",image:"Icon_Brokerage_Firms.png", totals2:"Total U.S. Financial Advisors", numbers2:"460,120", image2:"Icon_Financial_Advisors.png"},
+],
+});
