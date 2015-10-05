952845cd00e149476264c80370164bca83e99c1f
LarryHPham <larrypham1991@yahoo.com>
2015-10-05 18:52:37 -0500

    Revert "Revert "TASK-283(NFL - Team Frame - Daily Updates) files committed""
    This reverts commit 963cc6ce6d2f3212faf45444be92cae23d92e66c. (reverted from commit 4cf526dfdd0c835c249dadb56772a4683c4e85f8)

--- client/templates/modules/daily_update/daily_update.js
+++ client/templates/modules/daily_update/daily_update.js
@@ -1,166 +1,94 @@
-/* Author: Ryan Fisher
-Created: 08/17/2015
+/*Author: [Thanush Subramanian Elango]
+Created: [07/15/2015]
 Description: [daily_update]
 Associated Files: [daily_update.less][daily_update.html]*/
 
-var btncnt = 1;
-Template.daily_update.helpers({
-  team: 'Chicago Bears',
-
-  btnCount: function () {
-    return btncnt++;
-  },
-  city: 'Chicago',
-  wins: 5,
-  loss: 11,
-  fullyear: 2014,
-  updated: '4:00PM EST',
-  offense: '5,233',
-  defense: '6,033',
-  points: 318,
-  turnover: -5,
-  circle:[
-    {year: '07'},
-    {year: '08'},
-    {year: '09'},
-    {year: '10'},
-    {year: '11'},
-    {year: '12'},
-    {year: '13'},
-    {year: '14'},
-  ]
-});
-
-Template.daily_update.onRendered( function() {
-  $('#btnid8').addClass('active');
-});
-
-teamChangeYear = function(e,year){
-
-  var chart = $('#teamUpContainer').highcharts();
-  if(year == '14'){
-    console.log('14');
-    chart.series[0].update({ data: [264, 234, 278, 376]});
-    chart.series[1].update({ data: [311, 367, 443, 397]});
-  }
-  if(year == '13'){
-    console.log('13');
-    chart.series[0].update({ data: [274, 254, 288, 396]});
-    chart.series[1].update({ data: [211, 347, 423, 377]});
-  }
-  if(year == '12'){
-    console.log('12');
-    chart.series[0].update({ data: [294, 244, 286, 306]});
-    chart.series[1].update({ data: [211, 347, 323, 357]});
-  }
-  if(year == '11'){
-    console.log('11');
-    chart.series[0].update({ data: [274, 400, 288, 396]});
-    chart.series[1].update({ data: [211, 347, 300, 377]});
-  }
-  if(year == '10'){
-    console.log('10');
-    chart.series[0].update({ data: [274, 254, 350, 396]});
-    chart.series[1].update({ data: [211, 250, 423, 377]});
-  }
-  if(year == '09'){
-    console.log('09');
-    chart.series[0].update({ data: [275, 254, 288, 396]});
-    chart.series[1].update({ data: [211, 347, 400, 377]});
-  }
-  if(year == '08'){
-    console.log('08');
-    chart.series[0].update({ data: [230, 254, 288, 396]});
-    chart.series[1].update({ data: [211, 347, 423, 425]});
-  }
-  if(year == '07'){
-    console.log('07');
-    chart.series[0].update({ data: [274, 254, 288, 266]});
-    chart.series[1].update({ data: [281, 347, 423, 377]});
-  }
-  $('.all_team_update_modules-graphbutton_circle1').removeClass('active');
-  $('#'+e.id).addClass('active');
-}
 
 ///////Chart Creation ///////////
-
 Template.daily_update.dailyupdategraphh =  function(){
-  var url = ''; //"http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?";
+  var url =  "http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?";
   $.getJSON(url,  function(data) {
-    //options.series[0].data = data;
+    options.series[0].data = data;
     var chart = new Highcharts.Chart(options);
   });
   var options = {
-    chart: {
+    exporting:{
+      enabled:false
+    },
+    credits:{
+      enabled:false
+    },
+
+    scrollbar:{
+      enabled:false
+    },
+
+    rangeSelector: {
+      enabled:false,
+      buttonTheme: { // styles for the buttons
+        fill: 'none',
+        stroke: 'none',
+        'stroke-width': 0,
+        r: 8,
+        style: {
+          color: '#039',
+          fontWeight: 'bold'
+        },
+        states: {
+          hover: {
+          },
+          select: {
+            fill: '#039',
+            style: {
+              color: 'white'
+            }
+          }
+        }
+      },
+      inputBoxBorderColor: 'gray',
+      inputBoxWidth: 120,
+      inputBoxHeight: 18,
+      inputStyle: {
+        color: '#039',
+        fontWeight: 'bold'
+      },
+      labelStyle: {
+        color: 'silver',
+        fontWeight: 'bold'
+      },
+      selected: 1,
+    },
+    chart:{
       width:400,
-      height:120,
-      type: 'column'
+      height:110
     },
-    title:{
-        text: ''
+    title: {
+      text: ''
     },
-    legend:{
-        enabled: false
+    yAxis:{
+      opposite: true,
+      title:'',
+      labels:{
+        format: '${value}',
+      },
     },
     xAxis: {
-        categories: [
-            '@ Min',
-            'VS Det',
-            'VS NO',
-            'VS Dal'
-        ],
-        crosshair: true,
-        labels: {
-          style: {
-            color: '#999'
-          }
-        }
+      type:'datetime',
+      labels: {
+        enabled: false,
+      },
     },
-    yAxis: {
-        min: 200,
-        max: 500,
-        tickInterval: 100,
-        opposite: true,
-        title: {
-            text: ''
-        },
-        labels: {
-          style: {
-            color: '#999'
-          }
-        }
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0,
-            borderWidth: 0,
-            groupPadding: 0.15,
-            dataLabels: {
-                enabled: true,
-                inside: true,
-                color: '#FFFFFF',
-                allowOverlap: true,
-                style:{"fontWeight": "normal" }
-            },
-            enableMouseTracking: false
-        },
-        series: {
-            //pointWidth: 30
-        }
-    },
-    colors: [
-        '#0b162a',
-        '#f26f26'
-    ],
     series: [{
-        pointWidth: 30,
-        name: 'Chicago',
-        data: [264, 234, 278, 376]
+      name: 'National',
+      data:'data',
+      type: 'spline',
+      showInLegend: false,
+    },
+    {
+      name:'Chicago',
+      type:'spline',
+      showInLegend:false,
 
-    }, {
-        pointWidth: 30,
-        name: 'Opponent',
-        data: [311, 367, 443, 397]
     }]
   };
   return options;
