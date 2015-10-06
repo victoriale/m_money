c5b2a25ce06588e2f135cd8c1c61f2e4d29b24b2
navyae4 <navya@passfail.com>
2015-10-06 17:19:50 -0500

    task-434

---
+++ client/templates/modules/recommend/recommend_logic.js
@@ -0,0 +1,10 @@
+/* Author: Ryan Fisher
+** Created: 08/24/2015
+** Description: .less file for Recommend
+** Associated Files: recommend.html, recommend.less, recommend_logic.js
+*/
+
+Template.recommend.helpers({
+  like: 'InvestKit',
+  recommend: 'The InvestKit Magazine'
+})
