efc1322255155ce11ad62b399f7bf1ce30137464
LarryHPham <larrypham1991@yahoo.com>
2015-10-08 15:00:28 -0500

    plugged in new data and fixed some issues

--- client/templates/modules/money_memory/money_memory_logic.js
+++ client/templates/modules/money_memory/money_memory_logic.js
@@ -4,12 +4,53 @@
 ** Associated Files: money_memory.html, money_memory.less, money_memory_logic.js
 */
 
+Template.money_memory.onRendered(function(){
+  moneymemorygraph();
+  this.autorun(function(){
+  })
+});
+
+Template.money_memory.helpers({
+  companyInfo: function(){
+    var data = Session.get('daily_update');
+    if(typeof data == 'undefined'){
+      return '';
+    }
+    //data is being returned as string so convert to numbers and round to fit design
+    data['csi_percent_change_since_last'] = Number(data['csi_percent_change_since_last']).toFixed(2);
+    data['csi_price'] = Number(data['csi_price']).toFixed(2);
+    data['csi_price_change_since_last'] = Number(data['csi_price_change_since_last']).toFixed(2);
+    return data;
+  },
+
+  company: "Apple, Inc.",
+  stck_prc: "109.34",
+  stck_chng: "#ca1010",
+  stck_nums: "-3.42 (-3.03%)",
+  arrow: "arrow-down",
+  earn_lose: "earned",
+  pot_amount: "12,950.72",
+  totl_inc: "+46.2",
+  amount: "#22a922",
+  chng: "risen",
+  comp_bgn_date: "01/03/11",
+  intl_invstmnt: "10,000",
+  strt_date: "03/01/15",
+  end_date: "03/19/15"
+
+});
+
+
 //Function to render the spline chart
 function moneymemorygraph() {
+  var graphData = Session.get('money_memory');
+  var companyData = Session.get('profile_header');
+  var stockData = graphData.stock_history;
+
   var seriesOptions = [],
   seriesCounter = 0,
   //Placeholder data labels
-  names = ['', 'AAPL', ''],
+  names = 'AAPL'
 
   //Chart options
   createChart = function () {
@@ -39,7 +80,7 @@ function moneymemorygraph() {
       },
 
       tooltip: {
-        pointFormat: "Value: ${point.y:.2f}"
+        pointFormat: companyData.c_ticker+": ${point.y:.2f}"
       },
 
       yAxis: {
@@ -75,7 +116,7 @@ function moneymemorygraph() {
         enabled:false
       },
       series:
-      seriesOptions,
+      stockData,
       marker: {
         enabled: true
       }
@@ -83,44 +124,16 @@ function moneymemorygraph() {
   };
 
   //Populate chart with data
-  $.each(names, function (i, name) {
 
     $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
-      seriesOptions[i] = {
+      var seriesOptions = {
         name: name,
         data: data,
         type:'spline',
         marker: {symbol: 'circle'}
       };
 
-      // As we're loading the data asynchronously, we don't know what order it will arrive. So
-      // we keep a counter and create the chart when all the data is loaded.
-      seriesCounter += 1;
-
-      if (seriesCounter === names.length) {
-        createChart();
-      }
+      createChart();
     });
-  });
-}
 
-Template.money_memory.rendered=function() {
-  moneymemorygraph();
 }
-
-Template.money_memory.helpers({
-  company: "Apple, Inc.",
-  stck_prc: "109.34",
-  stck_chng: "#ca1010",
-  stck_nums: "-3.42 (-3.03%)",
-  arrow: "arrow-down",
-  earn_lose: "earned",
-  pot_amount: "12,950.72",
-  totl_inc: "+46.2",
-  amount: "#22a922",
-  chng: "risen",
-  comp_bgn_date: "01/03/11",
-  intl_invstmnt: "10,000",
-  strt_date: "03/01/15",
-  end_date: "03/19/15"
-});
