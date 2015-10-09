3a47ec3b9338c8cf81387a9f777a35376c1e03ce
Prashanth <prashanth@passfail.com>
2015-10-09 12:15:21 -0500

    TASK-443(area_breakdown) files committed
    area_breakdown.html, area_breakdown.less, area_breakdown.js files
    modified

--- client/templates/modules/area_breakdown/area_breakdown.js
+++ client/templates/modules/area_breakdown/area_breakdown.js
@@ -1,23 +1,38 @@
 /*
-ate: 07/21/2015
-Description: Gives Details about company Area brakdown statistics like location,population
-details,Average Rent,Description about the public market ,compensation,Total company shares
-It gives location,Nearby cities,Statistics
-Associated files: module.less,area_breakdown.less,area_breakdown.html,navigation_layout.less*/
-
-
-  Template.area_breakdown.helpers({
+Author : David Wibowo
+Date: 09/08/2015
+Description: Area brekdown
+Associated files: area_breakdown.html,area_breakdown.less,area_breakdown.js
+*/
+var full_page = false;
+Template.area_breakdown.helpers({
+   City:"San Francisco",
+   State:"CA",
+   LastUpdate:"Today at 8:00AM EST",
    items:[
-     {money:"245.84 Billion",desci:"Total Public Market Cap",fntawwsm:"fa-users"},
-     {money:"$873 Million",desci:"Total Executive Compensation",fntawwsm:"fa-globe"},
-     {money:"$1.3 Million",desci:"Average Executive Compensation",fntawwsm:"fa-user"},
-     {money:"44.53 Million",desci:"Total Shares",fntawwsm:"fa-building-o"},
+     {money:"4 Trillion", desci:"Total Public Market Cap", fa:"fa-users", url:"#"},
+     {money:"$7 Billion",desci:"Total Executive Compensation",fa:"fa-globe", url:"#"},
+     {money:"$537,500",desci:"Average Executive Compensation",fa:"fa-user", url:"#"},
+     {money:"44.53 Million",desci:"Total Shares",fa:"fa-building-o", url:"#"}
+   ]
+});
 
-   ],
+Template.area_breakdown.onRendered(function(){
+  this.autorun(function(){
+     // Map
+     map = new google.maps.Map(document.getElementById('loc_map'), {
+       zoom: 11,
+       center: {lat: 37.7833, lng: -122.4167},  // Latitude and longtitude in decimal form
+       scrollwheel: false
+     });
 
-   tile:[
-     {name:"Map",fnt:"fa-thumb-tack"},
-     {name:"Nearby Cities",fnt:"fa-crosshairs"},
-     //{name:"Statistics",fnt:"url('icon_statistics_custom.png')"}
- ]
-    });
+     // Enable full page or short view
+     if (full_page == true) {
+       $(".ftr").css("display","none");
+       $(".bkdn").css({"height":"auto", "overflow":"visible"});
+     } else {
+       $(".ftr").css("display","block");
+       $(".bkdn").css({"height":"660px", "overflow":"hidden"});
+     }
+  });
+})
