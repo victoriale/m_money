338daaab80f003eda6b1d54c3632e7703eb5c50a
Larry Pham <larrypham1991@yahoo.com>
2015-10-14 05:06:56 -0500

    added project history and fixes to router.js

--- client/templates/modules/project_history/project_history.js
+++ client/templates/modules/project_history/project_history.js
@@ -7,6 +7,39 @@ Associated Files: project_history.html, project_history.less
 
 //This function should, once the page is rendered, set the session data "project_history_list" to an array of objects.
 //These objects should contain data about the various companies this module is supposed to display.
+
+Template.project_history.onCreated( function() {
+  this.autorun(function(){
+    //take all the data and put all the usable ones into a new array of useable content
+    var data = Session.get('work_history');
+    //console.log("Initial Data", data);
+    if(typeof data != 'undefined'){
+      var companies = data.companies;
+      if(typeof companies != 'undefined'){
+        var projArray = [];
+        for (id in companies){
+          var compList = companies[id];
+          var comp = {};
+          comp['location'] = compList['company_data'].c_hq_city + ", " + compList['company_data'].c_hq_state;
+          comp['c_name'] = compList['company_data'].c_name;
+          comp['c_ticker'] = compList['company_data'].c_ticker;
+          comp['c_id'] = compList['company_data'].c_id;
+          comp['exec_nearest_pos'] = compList['officer_positions'][0];
+          comp['connections'] = compList['connections'];
+          comp['o_id'] = data['officer_data'].o_id;
+          console.log("Converted Data", comp);
+          projArray.push(comp);
+        }
+        Session.set('new_project_history', projArray);
+      }else{
+        Session.set('new_project_history', '');
+        return '';
+      }
+    }
+  })
+
+});
+
 Template.project_history.onRendered( function() {
   Session.set("project_history_list",
   [
@@ -41,6 +74,24 @@ Template.project_history.onRendered( function() {
 var backgroundStyle = "white"; //Tracks the background color of the company tiles
 Template.project_history.helpers (
   {
+
+    moduleCheck: function(){
+      var check = Session.get('work_history');
+      if(typeof check == 'undefined'){
+        return '';
+      }
+      return check;
+    },
+
+    projList: function(){
+      var data = Session.get('new_project_history');
+      if(typeof data == 'undefined'){
+        return '';
+      }
+      return data;
+    },
+
+
     //getProjectList returns the structure in Session.get("project_history_list")
     //The project_history.html file is expecting an array of objects containing data about the various projects/companies.
     getProjectList: function() {
