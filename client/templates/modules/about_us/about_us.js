76c54ee748ef67599c6a46417e747f2e9f85c1be
LarryHPham <larrypham1991@yahoo.com>
2015-10-13 19:32:00 -0500

    about us module links to disclaimer pages

---
+++ client/templates/modules/about_us/about_us.js
@@ -0,0 +1,12 @@
+Template.about_us.helpers({
+  aboutus:function(){
+    return Router.path('content.aboutus');
+  },
+  contactus:function(){
+    return Router.path('content.contactus');
+  },
+  disclaimer:function(){
+    return Router.path('content.disclaimer');
+  },
+
+})
