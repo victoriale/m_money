0803f1989b7608c343e462c5b55a6b532f30bf2f
navyae4 <navya@passfail.com>
2015-10-15 12:45:49 -0500

    task- 456

---
+++ client/templates/webpages/about_exec_page/about_exec_page_logic.js
@@ -0,0 +1,40 @@
+/*
+  Author: Jerek Shoemaker
+  Created: 10/14/2015
+  Description: about_exec_page webpage
+  Associated Files: about_exec_page.html, about_exec_page.less, about_exec_page_logic.js
+*/
+
+Template.about_exec_page.helpers({
+  upd: "10/24/2014, 12:36 PM EDT",
+  location: "The United States of America",
+  exec: "[Person's Profile Name]",
+  desc1: " attended Harvard Univer- sity in 2010 and earned his MBA after 5 years of studying. ",
+  desc2: " attended school with ",
+  rival1: "[College Rival 1]",
+  rival2: "[College Rival 2]",
+  rival3: "[College Rival 3]",
+  dt: "07/29/2015",
+  bigdesc1: "Lorem ipsum dolor sit amet, tale homero dolores at pro, eam eu graeco civibus "
+          + "antiopam. Ut congue quaeque iudicabit nec, duo ut tollit electram. Eum ludus "
+          + "corpora ne, te erat aperiam vel, sit ei accumsan epicurei temporibus. Cu eos "
+          + "dolorem laboramus. Heabeo congue has ad, ea etiam suscipit eos, duo an fabellas "
+          + "salutatus comperhensam. Nisl illud an vix.",
+  bigdesc2: "Ut duo docendi qualisque, fugit principes evertitur an has, meis aliquam "
+          + "reprimque ei cum. Eu eum vero magna quodsi, pri ex nobis salutandi, qui id "
+          + "homero quaestio. In cum quodsi fastidii platonem. Vis eu percipit antiopam "
+          + "argumentum, nam ad eius mandamus. Ut duo hinc accumsan petentium.",
+  bigdesc3: "Mei ea quando utinam, everti accusam consetetur te sed, ea suas stet expentenda "
+          + "sed. His augue everti molestiae ne. Est alia suavvitate an, cu quot affert suspcipit "
+          + "est. Eum hinc mazim theophrastus eu, nec quod corpora consulatu no. Eirmod "
+          + "delicatissimi cum ut. Sea lorem audire adolescens an, id homero graeci labore cum, "
+          + "etiam munere convenire cu sit.",
+  bigdesc4: "lus ridens qualisque theophrastus ea. In eos audiam platoneum splendide, mei ei dicit "
+          + "paulo tempor, ut mea harum voluptua. Eos ei odio vulputate, melius persecuti id vel. "
+          + "Pre ei primis posidonium. Molestie sententiae scripserit et sed, at dolore pertinacia has, "
+          + "sed id novum postea assueverit.",
+  bigdesc5: "Sed patrioque intellegam complectitur ei, eos cu ferri suscipiantur. Pro putant fuisset "
+          + "pericula at, purto veniam docendi mei no. Imperdiet euripidis pri at, te quas dicam molestiae "
+          + "cum, vitae indoc- tum philosophia at sed. Sea latine atomorum et, eos inermis facilisis te. Ne "
+          + "sea simul exerci tritani, at sed pruto indoctum delicatissimi."
+});
