dcd9e7cb015a207b0625c4e89692a36954ca8909
navyae4 <navya@passfail.com>
2015-10-16 13:35:33 -0500

    task-465

--- client/templates/webpages/about_exec_page/about_exec_page_logic.js
+++ client/templates/webpages/about_exec_page/about_exec_page_logic.js
@@ -1,20 +1,107 @@
 /*
-  Author: Jerek Shoemaker
-  Created: 10/14/2015
+  Author: Sri Sindhusha Kuchipudi
+  Created: 10/15/2015
   Description: about_exec_page webpage
   Associated Files: about_exec_page.html, about_exec_page.less, about_exec_page_logic.js
 */
 
+//get data from Session.key
+var data = Session.get('profile_header');
+//Helpers to display dynamic data from session
 Template.about_exec_page.helpers({
-  upd: "10/24/2014, 12:36 PM EDT",
+//url to link to the executive page from top bar and view profile
+  Execurl: function() {
+    var data = Session.get("profile_header");
+    if(typeof data == "undefined")
+    {
+      return '';
+    }
+    var urlid= data['o_id'];
+     return Router.path('content.executiveprofile',{
+      partnerid: null,
+      exec_id: urlid
+    });
+  },
+  //to get and display first name
+  fname: function() {
+    var data = Session.get('profile_header');
+    if(typeof data == "undefined")
+    {
+      return '';
+    }
+    var name = data['o_first_name'];
+    return name;
+    },
+    //to get and display last name
+    lname: function() {
+      var data = Session.get('profile_header');
+      if(typeof data == "undefined")
+      {
+        return '';
+      }
+      var name = data['o_last_name'];
+      return name;
+    },
+  //to get and display only date
+  dt : function()
+   {
+     var data = Session.get('profile_header');
+     if(typeof data == "undefined")
+     {
+       return '';
+     }
+     var inputDate = data['o_last_updated'];
+     var date = new Date(inputDate);
+     if (!isNaN(date.getTime())) {
+     var day = date.getDate().toString();
+     var month = (date.getMonth() + 1).toString();
+     return (month[1] ? month : '0' + month[0]) + '/' +
+     (day[1] ? day : '0' + day[0]) + '/' +
+     date.getFullYear();
+  }
+},
+    //to get and display last updated date
+    upd:function(UNIX_timestamp){
+      var data = Session.get('profile_header');
+      if(typeof data == "undefined")
+      {
+        return '';
+      }
+      var inputDate = data['o_last_updated'];
+      var a = new Date(inputDate);
+      var year = a.getFullYear();
+      var month = a.getMonth()+1;
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes();
+      var sec = a.getSeconds();
+      var time = ((month.toString().length===1)?('0'+month):month) + '/' + ((date.toString().length===1)?('0'+date):date) + '/' + year+ ' '+'at'+ ' ' + ((hour>12)?(hour-12):hour) + ':' + ((min.toString().length===1)?('0'+min):min) +' '+ ((hour>12)?('pm'):'am') ;
+      return time;
+    },
+    //the data in about section
+    bio: function() {
+      var data = Session.get('profile_header');
+      if(typeof data == "undefined")
+      {
+        return '';
+      }
+      var bio = data['o_bio'];
+      return bio;
+    },
+    //to get and display pic
+    pic: function() {
+      var data = Session.get('profile_header');
+      if(typeof data == "undefined")
+      {
+        return '';
+      }
+      var pic = data['o_pic'];
+      return pic;
+      },
+
+//to get and display Location details
   location: "The United States of America",
-  exec: "[Person's Profile Name]",
-  desc1: " attended Harvard Univer- sity in 2010 and earned his MBA after 5 years of studying. ",
-  desc2: " attended school with ",
-  rival1: "[College Rival 1]",
-  rival2: "[College Rival 2]",
-  rival3: "[College Rival 3]",
-  dt: "07/29/2015",
+  //to get and display paragraph data
   bigdesc1: "Lorem ipsum dolor sit amet, tale homero dolores at pro, eam eu graeco civibus "
           + "antiopam. Ut congue quaeque iudicabit nec, duo ut tollit electram. Eum ludus "
           + "corpora ne, te erat aperiam vel, sit ei accumsan epicurei temporibus. Cu eos "
