bb4b4fe3ba1f076ee9b89378c403ef016fd05f57
William <willyk95@gmail.com>
2015-10-20 15:55:20 -0500

    Fix router sending everything to server

--- server/router.js
+++ server/router.js
@@ -53,7 +53,7 @@ var batch_envar = new Meteor.EnvironmentVariable;
 
 // Filter out bot requests
 var seoPicker = Picker.filter(function(req, res) {
-  return true;
+  // return true;
   if ( /bot/.test(req.headers['user-agent']) || /Webmaster/.test(req.headers['user-agent']) || /Bing/.test(req.headers['user-agent']) || /externalhit/.test(req.headers['user-agent']) ) {
     return true;
   }
