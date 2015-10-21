b3e77a6136010ac017d8505674e5d61c08fb28c2
LarryHPham <larrypham1991@yahoo.com>
2015-10-20 23:33:54 -0500

    profile header fixes

--- client/templates/modules/c_p_header/c_p_header.js
+++ client/templates/modules/c_p_header/c_p_header.js
@@ -113,3 +113,182 @@ Template.cp_rdr.helpers({
     return data;
   },
 });
+
+Template.c_p_graph.helpers({
+  buttons: function(){
+    var buttons = [
+      {data:"1D"},
+      {data:"5D"},
+      {data:"10D"},
+      {data:"1M"},
+      {data:"3M"},
+      {data:"6M"},
+      {data:"9M"},
+      {data:"1Y"},
+      {data:"3Y"},
+      {data:"5Y"},
+      {data:"10Y"},
+    ];
+    return buttons;
+  },
+
+  //Helper to determine chart
+  getGraph: function(){
+    var data = Session.get('graph_data');
+    var c_p_range = Session.get('c_p_range');
+
+    //If data does not exists exit helper
+    if(typeof data === 'undefined'){
+      return ''
+    }
+
+    //Get dependencies to find date range
+    var dataLength = data.highchartsData.length;
+    var latestDate = moment(data.highchartsData[dataLength - 1][0]);
+    //Get range value based on option selected
+    switch(c_p_range){
+      case '1D':
+        var range = 1;
+        var min = latestDate.subtract(1, 'days').format('X') * 1000;
+      break;
+      case '5D':
+        var range = 5;
+        var min = latestDate.subtract(5, 'days').format('X') * 1000;
+      break;
+      case '10D':
+        var range = 10;
+        var min = latestDate.subtract(10, 'days').format('X') * 1000;
+      break;
+      case '1M':
+        var range = 30;
+        var min = latestDate.subtract(1, 'months').format('X') * 1000;
+      break;
+      case '3M':
+        var range = 90;
+        var min = latestDate.subtract(3, 'months').format('X') * 1000;
+      break;
+      case '6M':
+        var range = 180;
+        var min = latestDate.subtract(6, 'months').format('X') * 1000;
+      break;
+      case '9M':
+        var range = 270;
+        var min = latestDate.subtract(9, 'months').format('X') * 1000;
+      break;
+      case '1Y':
+        var range = 365;
+        var min = latestDate.subtract(1, 'years').format('X') * 1000;
+      break;
+      case '3Y':
+        var range = 1095;
+        var min = latestDate.subtract(3, 'years').format('X') * 1000;
+      break;
+      case '5Y':
+        var range = 1825;
+        var min = latestDate.subtract(5, 'years').format('X') * 1000;
+      break;
+      case '10Y':
+        var range = 3650;
+        var min = latestDate.subtract(10, 'years').format('X') * 1000;
+      break;
+      default:
+        var range = 3650;
+      break;
+    }
+
+    //Get oldest date available to check if data range is possible
+    var oldestDate = moment(data.highchartsData[0][0]).format('X') * 1000;
+    //If min is less than oldest data available, set min to oldest date
+    if(min <= oldestDate){
+      min = oldestDate;
+    }
+    var cfoGraphObject = {
+      title: {
+          text: ''
+      },
+      chart: {
+          type: 'spline',
+          events: {
+              redraw: function() {}
+          }
+      },
+      xAxis: {
+          type: 'datetime',
+          labels: {
+              overflow: 'justify'
+          },
+          min: min
+      },
+      yAxis: {
+          title: '',
+          floor: 0,
+          gridLineDashStyle: 'longdash',
+          minTickInterval: 5,
+          plotLines: [{
+              value: 0,
+              width: 1,
+              color: '#808080'
+          }],
+          labels: {
+              formatter: function() {
+                  return '$' + this.value
+              }
+          }
+      },
+      tooltip: {
+        pointFormat: "Value: ${point.y:.2f}"
+      },
+      plotOptions: {
+          spline: {
+              lineWidth: 2,
+              states: {
+                  hover: {
+                      lineWidth: 3
+                  }
+              },
+              marker: {
+                  enabled: false
+              },
+              pointInterval: 3600000, // one hour
+              pointStart: Date.UTC(2015, 4, 31, 0, 0, 0)
+          }
+      },
+      legend: {
+        enabled: false
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+          name: data.c_name,
+          data: data.highchartsData
+      }]
+    }
+
+    return cfoGraphObject;
+  },
+});
+Template.c_p_graph.events({
+  'click .c_p_graph-buttons-circle': function(e, t){
+    Session.set('c_p_range', e.currentTarget.id);
+  },
+})
+
+Template.c_p_graph.onCreated(function(){
+  this.autorun(function(){
+    var data = Session.get('daily_update');
+    var highchartsData = [];
+
+    data.stock_hist.forEach(function(item, index){
+      //Transform date
+      var date = moment(item.sh_date).format('X') * 1000;
+      //Build point array
+      var point = [date, Number(item.sh_close)]
+      //Push point array to data set
+      highchartsData.push(point);
+    })
+    data.highchartsData = highchartsData;
+
+    Session.set('graph_data', data);
+  })
+})
