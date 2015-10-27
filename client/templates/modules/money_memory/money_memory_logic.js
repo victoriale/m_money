56aebfe4910544d8c19e89fbe8ec24c0bdad6b57
lutzklai <lutzklai@gmail.com>
2015-10-27 16:52:45 -0500

    added error message for edge case of money memory module and page. (If data doesnt exist for selected time

--- client/templates/modules/money_memory/money_memory_logic.js
+++ client/templates/modules/money_memory/money_memory_logic.js
@@ -110,6 +110,12 @@ Template.mm_end_date.onRendered(function(){
 })
 
 Template.money_memory.helpers({
+  //Helper to determine if result exists (Fix for if no stock data for time range). If results DNE, show error message
+  resultExists: function(){
+    var data = Session.get('money_memory');
+
+    return typeof data === 'undefined' ? false : true;
+  },
   //Helper to determine URL to money memory page
   linkToMM: function(){
     var params = Router.current().getParams();
