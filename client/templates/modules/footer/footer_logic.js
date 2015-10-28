50c3c3a7868c377a86836f3a09725aa7ac1fb4eb
victoriale <le.victoriavan@gmail.com>
2015-10-28 15:36:43 -0500

    some changes for header, footer, and homepage

--- client/templates/modules/footer/footer_logic.js
+++ client/templates/modules/footer/footer_logic.js
@@ -4,20 +4,22 @@ Template.footer.onRendered(function(){
     var URL = window.top.location.href;
 
     String_facebook ="https://www.facebook.com/sharer/sharer.php?u="+ URL;
-    $(".facebook-link").attr("href", String_facebook);
+    $(".facebook_foot4").attr("href", String_facebook);
 
     //for twitter sharing we are appending the URL with the static URL
     String_twitter ="https://twitter.com/home?status="+ URL;
-    $(".twitter-link").attr("href", String_twitter);
+    $(".facebook_foot3").attr("href", String_twitter);
 
     //for google+ sharing we are appending the URL with the static URL
     String_google ="https://plus.google.com/share?url="+ URL;
-    $(".google-link").attr("href", String_google);
+    $(".facebook_foot1").attr("href", String_google);
 
     //for pinterest sharing we are appending the URL with the static URL
     String_pinterest ="https://pinterest.com/pin/create/button/?url=&media="+ URL;
-    $(".pinterest-link").attr("href", String_pinterest);
-
+    $(".facebook_foot").attr("href", String_pinterest);
+    //for linkedin sharing we are appending the URL with the static URL
+    String_linkedin = "https://www.linkedin.com/";
+    $(".facebook_foot2").attr("href", String_linkedin);
 })
 
 Template.footer.helpers({
