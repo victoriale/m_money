ad28b687e7cdbc157e5211790a1d3840eea0ff48
LarryHPham <larrypham1991@yahoo.com>
2015-10-31 17:51:43 -0500

    search

--- client/templates/webpages/search_page/search_page.js
+++ client/templates/webpages/search_page/search_page.js
@@ -2,6 +2,22 @@ Template.search_page.onRendered(function(){
   this.autorun(function(){
     var searchParams = Router.current().getParams();
     console.log(searchParams.search_results);
+    Meteor.call("GetSuggestion", searchParams.search_results, time,  function(error, data){
+      if(error){
+        console.log('Invalid Team Error',error);
+        Session.set('IsError',true);
+        return '';
+      }
+      console.log(data);
+      for ( var module_name in data ) {
+        if ( data.hasOwnProperty(module_name) ) {
+          console.log(module_name,data[module_name]);
+          Session.set(module_name,data[module_name]);
+        }
+      }
+    });
+
+
   })
 })
 
@@ -14,4 +30,26 @@ Template.search_page.events({
 
 Template.search_page.helpers({
 
+  Results: function(){
+    var allResults = Session.get('data');
+    if(typeof allResults == 'undefined'){
+      return '';
+    }
+    allResults['NewList'] = {};
+
+    allResults['NewList']['executive'] = [];
+    allResults['NewList']['company'] = [];
+    allResults['NewList']['location'] = [];
+    $.map(allResults['name']['func_data']['search_data'], function(data, index){
+      if(data.name_type == 'officer'){
+        console.log('push exec');
+        allResults['NewList']['executive'].push(data);
+      }
+      if(data.name_type == 'company'){
+        console.log('push company');
+        allResults['NewList']['company'].push(data);
+      }
+    })
+    console.log(allResults);
+  },
 })
