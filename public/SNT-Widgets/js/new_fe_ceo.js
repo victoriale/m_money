2dd2a8508f9d2769576a6fcc68f46dbe795e5d5c
victoriale <le.victoriavan@gmail.com>
2015-11-04 12:01:59 -0600

    ad. added

--- public/SNT-Widgets/js/new_fe_ceo.js
+++ public/SNT-Widgets/js/new_fe_ceo.js
@@ -19,7 +19,7 @@ $(function(){
       }
   });
 
-	$.get('http://apifin.synapsys.us/call_controller.php?action=widget&option=highest_paid_female_ceo', function(data){
+	$.get('http://apifin.investkit.com/call_controller.php?action=widget&option=highest_paid_female_ceo', function(data){
     data_result = data.highest_paid_female_ceo;
     curData = data_result.list_data;
     dataLength = curData.length;
@@ -27,14 +27,16 @@ $(function(){
   }, 'json')
 })//END OF FUNCTION
 function dataCall(index){
-  console.log(curData);
-
   $('.exec-link').attr('href',"http://www.investkit.com/"+curData[index].o_first_name+"-"+curData[index].o_last_name+"/"+curData[index].c_ticker+"/executive/"+curData[index].o_id);
   $('.fcw-href').attr('href',"http://www.investkit.com/"+compUrlName(data_result.list_title)+"/female_ceo/executive-list");
-  $('.fcw-t2-title').html(curData[index].c_name);
+  $('.fcw-t2-title').html(curData[index].c_ticker);
+  $('.fcw-loc').html(curData[index].c_hq_city+' '+curData[index].c_hq_state);
   $('.fcw-content1').html(curData[index].o_first_name+' '+curData[index].o_last_name);
   $('#paid').html(nFormatter(curData[index].TotalComp));
-  $('.fcw-image').css('background','url(http://apifin2.synapsys.us/images/'+curData[index].o_pic+') no-repeat');
+  $('.fcw-image').css('background','url('+imageUrl(curData[index].o_pic)+') no-repeat');
+  $('#title_link').attr('href',"http://www.investkit.com/"+curData[index].c_ticker+"/"+compUrlName(curData[index].c_name)+"/company/"+curData[index].c_id);
+
+  $('#loc_link').attr('href',"http://www.investkit.com/"+curData[index].c_hq_state+"/location");
 }
 
 function compUrlName(company) {
@@ -56,4 +58,10 @@ function nFormatter(num) {
 		return (num / 1000).toFixed(1).replace(/\.0$/, '') + ' K';
 	}
 	return num;
+}
+function imageUrl(path){
+  if(typeof path == 'undefined' || path == null || path == '' || path == 'null'){
+    return '/no_image.png';
+  }
+  return 'http://images.investkit.com/images/' + path;
 }
