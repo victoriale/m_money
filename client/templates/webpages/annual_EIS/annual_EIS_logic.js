0d9f871b7e7c753ce438c120ca0edaad3b8e2565
navyae4 <navya@passfail.com>
2015-11-05 16:50:02 -0600

    annual_EIS

--- client/templates/webpages/annual_EIS/annual_EIS_logic.js
+++ client/templates/webpages/annual_EIS/annual_EIS_logic.js
@@ -5,17 +5,68 @@
 */
 
 Template.annual_EIS.onCreated(function(){
-  Meteor.http.get('http://apifin.synapsys.us/call_controller.php?action=company_page&option=income_statement&param=3330',
+  var id = Session.get("profile_header").c_id;
+  Meteor.http.get('http://apifin.synapsys.us/call_controller.php?action=company_page&option=income_statement&param=' + id,
     function(error, data){
     Session.set("annual_EIS",data);
+    annualEISgraph();
+    var chart = $('#annualEISgraph').highcharts();
+    var axisMax = chart.yAxis[0].max;
+    chart.series[2].update({
+      tooltip:{
+        pointFormatter: function() {
+          return "Profit Margin: " + (this.y / (axisMax / 100)).toFixed(2) + "%";
+        }
+      }
+    });
+    for(var i = 0; i<chart.series[2].data.length; i++){
+      chart.series[2].data[i].y *= axisMax;
+      chart.series[2].data[i].update();
+    }
   });
 })
 
-Template.annual_EIS.onRendered(function(){
-  annualEISgraph();
-})
+function graphRevData() {
+  var data = Session.get('annual_EIS');
+  var target = data.data.income_statement.income_data["Revenue"];
+  var out = [];
+  out[0] = parseFloat(target["2011"]);
+  out[1] = parseFloat(target["2012"]);
+  out[2] = parseFloat(target["2013"]);
+  out[3] = parseFloat(target["2014"]);
+  out[4] = parseFloat(target["2015"]);
+  return out;
+}
 
-function annualEISgraph() {
+function graphIncomeData() {
+  var data = Session.get('annual_EIS');
+  var target = data.data.income_statement.income_data["Net Income"];
+  var out = [];
+  out[0] = parseFloat(target["2011"]);
+  out[1] = parseFloat(target["2012"]);
+  out[2] = parseFloat(target["2013"]);
+  out[3] = parseFloat(target["2014"]);
+  out[4] = parseFloat(target["2015"]);
+  return out;
+}
+
+function graphNetProfitMarginData() {
+  var data = Session.get('annual_EIS');
+  var revenue = graphRevData();
+  var income = graphIncomeData();
+  var out = [];
+  out[0] = income[0] / revenue[0];
+  out[1] = income[1] / revenue[1];
+  out[2] = income[2] / revenue[2];
+  out[3] = income[3] / revenue[3];
+  out[4] = income[4] / revenue[4];
+  return out;
+}
+
+function annualEISgraph(){
+  var rev = graphRevData();
+  var income = graphIncomeData();
+  var margin = graphNetProfitMarginData();
  $('#annualEISgraph').highcharts({
    credits: {
      enabled: false
@@ -31,25 +82,32 @@ function annualEISgraph() {
    },
 
    yAxis: {
-     tickInterval: 2.5,
+     tickInterval: 1000,
+     title: "In Millions of USD"
    },
+
 
    series: [{
      type: 'column',
-     name: 'Debt',
-     data: [2.4, 2.6, 5, 7.3, 5.9],
+     name: 'Net Income',
+     data: income,
      showInLegend: false,
      color: '#434348 '
    }, {
      type: 'column',
-     name: 'Assets',
-     data: [4.2, 2.6, 2.6, 8.5, 3.1],
+     name: 'Revenue',
+     data: rev,
      showInLegend: false,
      color: '#3098FF '
    }, {
      type: 'spline',
-     name: 'Average',
-     data: [3, 2.67, 3, 6.33, 3.33],
+     tooltip:{
+      pointFormatter: function() {
+        return "Profit Margins: " + (this.y).toFixed(2) + "%";
+      }
+     },
+     name: 'Profit Margin',
+     data: margin,
      showInLegend: false,
      marker: {
        lineWidth: 2,
@@ -73,8 +131,50 @@ Template.annual_EIS.helpers({
     return data['data']['income_statement']['company_info']['c_tr_last_updated'];
   },
   location: function(){
-    return "The United States of America";
+    var data = Session.get('annual_EIS');
+    var city = data['data']['income_statement']['company_info']['c_hq_city'];
+    city = city.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+    var state = data['data']['income_statement']['company_info']['c_hq_state'];
+    return city + ", " + state;
   },
+
+  selectYear: function() {
+    return "2015";
+  },
+
+  ninc: function() {
+    var data = Session.get('annual_EIS');
+    var income = parseInt(data.data.income_statement.income_data["Net Income"]["2015"]);
+    if(income > 1000)
+    {
+      income /= 1000;
+      return "$" + income.toFixed(2) + " Billion";
+    }
+    return "$" + income + " Million";
+  },
+
+  rev: function() {
+    var data = Session.get('annual_EIS');
+    var income = parseInt(data.data.income_statement.income_data["Revenue"]["2015"]);
+    if(income > 1000)
+    {
+      income /= 1000;
+      return "$" + income.toFixed(2) + " Billion";
+    }
+    return "$" + income + " Million";
+  },
+
+  pm: function() {
+    var data = Session.get('annual_EIS');
+    var income = parseFloat(data.data.income_statement.income_data["Net Income"]["2015"]);
+    var rev = parseFloat(data.data.income_statement.income_data["Revenue"]["2015"]);
+    var out = (income / rev) * 100;
+
+    return out.toFixed(2) + "%";
+  },
+
   data: function(){
     var data = Session.get('annual_EIS');
     var borderArray = ["d","s","d","s","d","d","d","d","d","d","s","s","d","d","d","s","s","d","d","s","d","d","d","s","d","s","s","d","d","s","d","d","s","s","d","d","d","d","d","d","d","d","d","d","d","d","d","d","d","e"];
@@ -91,51 +191,66 @@ Template.annual_EIS.helpers({
       returnArray[i]['style5'] = "";
 
       //Set amounts
-      returnArray[i]['amnt1'] = incomedata[Object.keys(incomedata)[i]]['2015'];
+      returnArray[i]['amnt1'] = incomedata[Object.keys(incomedata)[i]]['2014'];
       //If no amount, set it to -
       if(returnArray[i]['amnt1'] == undefined){
         returnArray[i]['amnt1'] = "-";
       }
       //If amount is negative, color it red
-      else if(parseInt(returnArray[i]['amnt1']) != NaN){
-        if(parseInt(returnArray[i]['amnt1']) < 0){
-          returnArray[i]['style1'] += "color:#ca1010;";
+      else {
+        if(parseFloat(returnArray[i]['amnt1']) != NaN){
+          returnArray[i]['amnt1'] = parseFloat(returnArray[i]['amnt1']).toFixed(2);
+          if(parseFloat(returnArray[i]['amnt1']) < 0){
+            returnArray[i]['style1'] += "color:#ca1010  ;";
+          }
         }
       }
-      returnArray[i]['amnt2'] = incomedata[Object.keys(incomedata)[i]]['2014'];
+      returnArray[i]['amnt2'] = incomedata[Object.keys(incomedata)[i]]['2013'];
       if(returnArray[i]['amnt2'] == undefined){
         returnArray[i]['amnt2'] = "-";
       }
-      else if(parseInt(returnArray[i]['amnt2']) != NaN){
-        if(parseInt(returnArray[i]['amnt2']) < 0){
-          returnArray[i]['style2'] += "color:#ca1010;";
+      else {
+        if(parseFloat(returnArray[i]['amnt2']) != NaN){
+          returnArray[i]['amnt2'] = parseFloat(returnArray[i]['amnt2']).toFixed(2);
+          if(parseFloat(returnArray[i]['amnt2']) < 0){
+            returnArray[i]['style2'] += "color:#ca1010  ;";
+          }
         }
       }
-      returnArray[i]['amnt3'] = incomedata[Object.keys(incomedata)[i]]['2013'];
+      returnArray[i]['amnt3'] = incomedata[Object.keys(incomedata)[i]]['2012'];
       if(returnArray[i]['amnt3'] == undefined){
         returnArray[i]['amnt3'] = "-";
       }
-      else if(parseInt(returnArray[i]['amnt3']) != NaN){
-        if(parseInt(returnArray[i]['amnt3']) < 0){
-          returnArray[i]['style3'] += "color:#ca1010;";
+      else {
+        if(parseFloat(returnArray[i]['amnt3']) != NaN){
+          returnArray[i]['amnt3'] = parseFloat(returnArray[i]['amnt3']).toFixed(2);
+          if(parseFloat(returnArray[i]['amnt3']) < 0){
+            returnArray[i]['style3'] += "color:#ca1010  ;";
+          }
         }
       }
-      returnArray[i]['amnt4'] = incomedata[Object.keys(incomedata)[i]]['2012'];
+      returnArray[i]['amnt4'] = incomedata[Object.keys(incomedata)[i]]['2011'];
       if(returnArray[i]['amnt4'] == undefined){
         returnArray[i]['amnt4'] = "-";
       }
-      else if(parseInt(returnArray[i]['amnt4']) != NaN){
-        if(parseInt(returnArray[i]['amnt4']) < 0){
-          returnArray[i]['style4'] += "color:#ca1010;";
+      else {
+        if(parseFloat(returnArray[i]['amnt4']) != NaN){
+          returnArray[i]['amnt4'] = parseFloat(returnArray[i]['amnt4']).toFixed(2);
+          if(parseFloat(returnArray[i]['amnt4']) < 0){
+            returnArray[i]['style4'] += "color:#ca1010  ;";
+          }
         }
       }
-      returnArray[i]['amnt5'] = incomedata[Object.keys(incomedata)[i]]['2011'];
+      returnArray[i]['amnt5'] = incomedata[Object.keys(incomedata)[i]]['2010'];
       if(returnArray[i]['amnt5'] == undefined){
         returnArray[i]['amnt5'] = "-";
       }
-      else if(parseInt(returnArray[i]['amnt5']) != NaN){
-        if(parseInt(returnArray[i]['amnt5']) < 0){
-          returnArray[i]['style5'] += "color:#ca1010;";
+      else {
+        if(parseFloat(returnArray[i]['amnt5']) != NaN){
+          returnArray[i]['amnt5'] = parseFloat(returnArray[i]['amnt5']).toFixed(2);
+          if(parseFloat(returnArray[i]['amnt5']) < 0){
+            returnArray[i]['style5'] += "color:#ca1010  ;";
+          }
         }
       }
 
