3862a95cd74ecdfac54c3d70de9c824ec5390750
klawil <wklausmeyer@yahoo.com>
2015-11-10 18:09:47 -0600

    Remove debugging console logs for ad zone position

--- client/layouts/finance/ad_zone/ad_zone_logic.js
+++ client/layouts/finance/ad_zone/ad_zone_logic.js
@@ -3,18 +3,14 @@ Template.ad_zone.onRendered(function(){
     var y_stop = 10;
     var y_stop = $('.layout_nav').offset().top + $('.layout_nav').height();
     if ( $(window).scrollTop() >= ($('.ad_zone-widget1-area').offset().top - y_stop) && ($('.ad_zone-widget1-area').offset().top + $('.ad_zone-widget1-area').height() + 25) >= $('.footer-standard').offset().top ) {
-      console.log(1);
       $('.ad_zone-widget1-area').attr('style','');
       $('.ad_zone-widget1-area').css({position: 'absolute', bottom: '0px'});
     } else if ( $(window).scrollTop() > $('.ad_zone-widget1-area').offset().top || ($(window).scrollTop() <= ($('.ad_zone-widget1-area').offset().top - y_stop) && ($('.ad_zone-widget1-area').offset().top + $('.ad_zone-widget1-area').height() + 25) <= $('.footer-standard').offset().top && $(window).scrollTop() > y_stop) ) {
-      console.log(2);
       $('.ad_zone-widget1-area').attr('style','');
       $('.ad_zone-widget1-area').css({position: 'fixed', top: '10px'});
     } else if ( $(window).scrollTop() <= y_stop && $(window).scrollTop() < $('.ad_zone-widget1-area').offset().top ) {
-      console.log(3);
       $('.ad_zone-widget1-area').attr('style','');
     } else {
-      console.log(4);
     }
   })
 });
