e5f9f23b4072b4f2946c2ae88a5aa1cbd54f48de
William <willyk95@gmail.com>
2015-11-13 10:38:19 -0600

    Fix sidebar widget issue at the bottom of the page

--- client/layouts/finance/ad_zone/ad_zone_logic.js
+++ client/layouts/finance/ad_zone/ad_zone_logic.js
@@ -1,16 +1,15 @@
 Template.ad_zone.onRendered(function(){
   $(document).on('scroll',function() {
-    var y_stop = 10;
-    var y_stop = $('.layout_nav').offset().top + $('.layout_nav').height();
-    if ( $(window).scrollTop() >= ($('.ad_zone-widget1-area').offset().top - y_stop) && ($('.ad_zone-widget1-area').offset().top + $('.ad_zone-widget1-area').height() + 25) >= $('.footer-standard').offset().top ) {
+    var y_buffer = 10;
+    var y_top = $('.layout_nav').offset().top + $('.layout_nav').height();
+    if ( $(window).scrollTop() < $('.finance_body').offset().top + y_buffer ) {
       $('.ad_zone-widget1-area').attr('style','');
-      $('.ad_zone-widget1-area').css({position: 'absolute', bottom: '0px'});
-    } else if ( $(window).scrollTop() > $('.ad_zone-widget1-area').offset().top || ($(window).scrollTop() <= ($('.ad_zone-widget1-area').offset().top - y_stop) && ($('.ad_zone-widget1-area').offset().top + $('.ad_zone-widget1-area').height() + 25) <= $('.footer-standard').offset().top && $(window).scrollTop() > y_stop) ) {
+    } else if ( $(window).scrollTop() + $('.ad_zone-widget1-area').height() + (y_buffer * 2) > $('.footer-standard').offset().top ) {
       $('.ad_zone-widget1-area').attr('style','');
-      $('.ad_zone-widget1-area').css({position: 'fixed', top: '10px'});
-    } else if ( $(window).scrollTop() <= y_stop && $(window).scrollTop() < $('.ad_zone-widget1-area').offset().top ) {
-      $('.ad_zone-widget1-area').attr('style','');
+      $('.ad_zone-widget1-area').css({top: 'auto', bottom: y_buffer + 'px'});
     } else {
+      $('.ad_zone-widget1-area').attr('style','');
+      $('.ad_zone-widget1-area').css({position: 'fixed', top: y_buffer + 'px'});
     }
   })
 });
