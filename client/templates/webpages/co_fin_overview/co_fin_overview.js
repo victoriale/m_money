64f1b381416f5cfa6294514b0d75c019ac998d01
lutzklai <lutzklai@gmail.com>
2015-11-17 15:38:07 -0600

    change close to 4:00pm est

--- client/templates/webpages/co_fin_overview/co_fin_overview.js
+++ client/templates/webpages/co_fin_overview/co_fin_overview.js
@@ -70,20 +70,20 @@ Template.co_fin_overview.helpers({
 
         //If current day is saturday or sunday, set min to friday 9:30 AM else set to current weekday 9:30 AM
         if(current_day === 6|| current_day === 7){
-          var min = moment.utc().subtract(5, 'hours').endOf('isoweek').subtract(2, 'days').hour(14).minute(30).format('X') * 1000;
-          var max = moment.utc().subtract(5, 'hours').endOf('isoweek').subtract(2, 'days').hour(21).minute(30).format('X') * 1000;
+          var min = moment.utc().subtract(5, 'hours').endOf('isoweek').subtract(2, 'days').hour(14).minute(0).format('X') * 1000;
+          var max = moment.utc().subtract(5, 'hours').endOf('isoweek').subtract(2, 'days').hour(21).minute(0).format('X') * 1000;
         }else{
           //If current hour:minute is after 9:30 (Open) use today range, else use yesterday's values
           if(current_time > 930){
-            var min = moment.utc().subtract(5, 'hours').hour(14).minute(30).second(0).format('X') * 1000;
-            var max = moment.utc().subtract(5, 'hours').hour(21).minute(30).second(0).format('X') * 1000;
+            var min = moment.utc().subtract(5, 'hours').hour(14).minute(0).second(0).format('X') * 1000;
+            var max = moment.utc().subtract(5, 'hours').hour(21).minute(0).second(0).format('X') * 1000;
           }else{
-            var min = moment.utc().subtract(1, 'days').subtract(5, 'hours').hour(14).minute(30).second(0).format('X') * 1000;
-            var max = moment.utc().subtract(1, 'days').subtract(5, 'hours').hour(21).minute(30).second(0).format('X') * 1000;
+            var min = moment.utc().subtract(1, 'days').subtract(5, 'hours').hour(14).minute(0).second(0).format('X') * 1000;
+            var max = moment.utc().subtract(1, 'days').subtract(5, 'hours').hour(21).minute(0).second(0).format('X') * 1000;
           }
         }
 
-        var tickPositions = [min, min + ((3600 + 1800) * 1000), min + ((2 * 3600 + 1800) * 1000), min + ((3 * 3600 + 1800) * 1000), min + ((4 * 3600 + 1800) * 1000), min + ((5 * 3600 + 1800) * 1000), min + (7 * 3600 * 1000)];
+        var tickPositions = [min + ((1800) * 1000), min + ((2 * 3600) * 1000), min + ((3 * 3600) * 1000), min + ((4 * 3600) * 1000), min + ((5 * 3600) * 1000), min + ((6 * 3600) * 1000), min + ((7 * 3600) * 1000)];
 
         var xAxis_format = '%l:%M %P';
         var tooltip_format = '%l:%M %P EST';
@@ -169,10 +169,10 @@ Template.co_fin_overview.helpers({
               formatter: function(){
 
                 if(this.isFirst && fo_range === 'cfoBtn0'){
-                  return Highcharts.dateFormat(xAxis_format, this.value) + '<br>(Open)';
+                  return Highcharts.dateFormat(xAxis_format, this.value) + '<br>(Open EST)';
                 }
                 if(this.isLast && fo_range == 'cfoBtn0'){
-                  return Highcharts.dateFormat(xAxis_format, this.value) + '<br>(Close)';
+                  return Highcharts.dateFormat(xAxis_format, this.value) + '<br>(Close EST)';
                 }
 
                 return Highcharts.dateFormat(xAxis_format, this.value);
