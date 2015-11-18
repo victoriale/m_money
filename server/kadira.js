08cca90a0eaa6d8b239b6cdc6cc0f88409912220
William <willyk95@gmail.com>
2015-11-18 17:52:14 -0600

    Change kadira host name

--- server/kadira.js
+++ server/kadira.js
@@ -1,1 +1,1 @@
-Kadira.connect('bq29w6ABpbKFiYTuF', '469a5500-84b4-42bb-918e-6c3c62edc065', {hostname: 'Staging'});
+Kadira.connect('bq29w6ABpbKFiYTuF', '469a5500-84b4-42bb-918e-6c3c62edc065', {hostname: 'LIVE'});
