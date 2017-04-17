2504921270617997cdb9dd5a79dbe71b9615e037
larryhpham <larrypham1991@yahoo.com>
2017-04-17 11:34:32 -0500

    added more proper error checking
    (cherry picked from commit 2e7ffe6)
    
    (cherry picked from commit 389f110)

--- client/layouts/finance/left_ad_zone/left_ad_zone_logic.js
+++ client/layouts/finance/left_ad_zone/left_ad_zone_logic.js
@@ -17,40 +17,42 @@ Template.left_ad_zone.onRendered(function(){
   this.autorun(function(){
       try{
         if(typeof Session.get('p_data') != 'undefined'){
-          var nexstar = Session.get('p_data').corporate_name.toLowerCase();
-          var params = Router.current().getParams();
-          var nexstarAd = getDomainAds(params.partner_id);
-          if(nexstar == 'nexstar'){
-            $(document).ready(function() {
-              var info = getTagInfo();
-              //in globalfunc.js to grab nexstart alias script tag;
-              var script_tag = getScript(params.partner_id);
-              var alias = script_tag+'_passfail_skyscraper';
-              alias = alias !== '' ? 'alias=' + ( alias.split("_") ? alias.split("_")[ 0 ] : alias ) + '_' + info.alias : '';
-              // var script = '<scr'+'ipt src="http://' + info.domain + '.adtechus.com/addyn/3.0/5336.1/defaultplacementid/0/-1/ADTECH;' + alias + ';loc=100;target=_blank;grp=' + info.groupId + ';misc=' + new Date().getTime() + '"></scri'+'pt>'
-              var scriptUrl = 'http://' + info.domain + '.adtechus.com/addyn/3.0/5336.1/' + nexstarAd.skyscraper + '/0/-1/ADTECH;' + alias + ';loc=100;target=_blank;grp=' + info.groupId + ';misc=' + new Date().getTime();
-              //due to no access controller on their end decided to make a server side call to allow us to grab and parse out the tag_string
-              Meteor.call('nexstarMethod',scriptUrl,function(error,data){
-                if(data.statusCode == 304 || data.statusCode == 200){
-                  $(".ad_zone-placement").css('display','block');
-                  var tag_string = data.content;
-                  var skyscraperIframe = document.getElementById("ad_zone_iframe");
-                  var script = skyscraperIframe.contentWindow.document.createElement("script");
-                  script.type = "text/javascript";
-                  script.innerHTML = tag_string;
-                  skyscraperIframe.contentWindow.document.body.appendChild(script);
-                }else{
-                  $(".ad_zone-placement").css('display','none');
-                }
+          if(Session.get('p_data').corporate_name){
+            var nexstar = Session.get('p_data').corporate_name.toLowerCase();
+            var params = Router.current().getParams();
+            var nexstarAd = getDomainAds(params.partner_id);
+            if(nexstar == 'nexstar'){
+              $(document).ready(function() {
+                var info = getTagInfo();
+                //in globalfunc.js to grab nexstart alias script tag;
+                var script_tag = getScript(params.partner_id);
+                var alias = script_tag+'_passfail_skyscraper';
+                alias = alias !== '' ? 'alias=' + ( alias.split("_") ? alias.split("_")[ 0 ] : alias ) + '_' + info.alias : '';
+                // var script = '<scr'+'ipt src="http://' + info.domain + '.adtechus.com/addyn/3.0/5336.1/defaultplacementid/0/-1/ADTECH;' + alias + ';loc=100;target=_blank;grp=' + info.groupId + ';misc=' + new Date().getTime() + '"></scri'+'pt>'
+                var scriptUrl = 'http://' + info.domain + '.adtechus.com/addyn/3.0/5336.1/' + nexstarAd.skyscraper + '/0/-1/ADTECH;' + alias + ';loc=100;target=_blank;grp=' + info.groupId + ';misc=' + new Date().getTime();
+                //due to no access controller on their end decided to make a server side call to allow us to grab and parse out the tag_string
+                Meteor.call('nexstarMethod',scriptUrl,function(error,data){
+                  if(data.statusCode == 304 || data.statusCode == 200){
+                    $(".ad_zone-placement").css('display','block');
+                    var tag_string = data.content;
+                    var skyscraperIframe = document.getElementById("ad_zone_iframe");
+                    var script = skyscraperIframe.contentWindow.document.createElement("script");
+                    script.type = "text/javascript";
+                    script.innerHTML = tag_string;
+                    skyscraperIframe.contentWindow.document.body.appendChild(script);
+                  }else{
+                    $(".ad_zone-placement").css('display','none');
+                  }
+                });
               });
-            });
-          }//end if nexstar
-          else{
-            $('.finance_body_skyscraper').css('display','none');
-          }//end else
+            }//end if nexstar
+            else{
+              $('.finance_body_skyscraper').css('display','none');
+            }//end else
+          }
         }//end of p_data if statement
       }catch(e){
-        // console.log(e);
+        console.log(e);
       }
   })//end of autorun
 });
